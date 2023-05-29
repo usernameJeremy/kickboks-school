efa900d606b3b23828d7ffb37b8f579b5229d528
usernameJeremy <Jeremy_koster@outlook.com>
2023-05-29 12:42:35 +0200

    nog wat kleine dingen fiksen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,17 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
 import { ClassesComponent } from './classes/classes.component';
-import { GalleryComponent } from './gallery/gallery.component';
 import { ContactComponent } from './contact/contact.component';
 import { SponsorsComponent } from './sponsors/sponsors.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
   { path: 'classes', component: ClassesComponent },
-  { path: 'gallery', component: GalleryComponent },
   { path: 'contact', component: ContactComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'sponsors', component: SponsorsComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
 import { ClassesComponent } from './classes/classes.component';
-import { GalleryComponent } from './gallery/gallery.component';
 import { ContactComponent } from './contact/contact.component';
 import { SponsorsComponent } from './sponsors/sponsors.component';
 
@@ -14,9 +12,7 @@ import { SponsorsComponent } from './sponsors/sponsors.component';
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent,
     ClassesComponent,
-    GalleryComponent,
     ContactComponent,
     SponsorsComponent
   ],
